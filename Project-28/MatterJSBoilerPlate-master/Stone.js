dd6b1b3bbc4ae9b8c715444ff644a55b086be429
alishaaggarwal630 <68144419+alishaaggarwal630@users.noreply.github.com>
2021-02-13 13:11:18 +0530

    Add files via upload

---
+++ Project-28/MatterJSBoilerPlate-master/Stone.js
@@ -0,0 +1,28 @@
+class Stone {
+    constructor(x,y,radius){
+      var options={
+        isStatic:false,
+        'restitution':0,
+        'friction':1,
+        'density':1.2
+      }
+      this.x=x;
+      this.y=y;
+      this.radius=radius;
+      this.image = loadImage("stone.png");
+      this.body=Bodies.circle(this.x,this.y,this.radius,options);
+      World.add(world,this.body);
+    
+    }
+  
+    display(){
+      var pos=this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      fill("grey");
+      imageMode(CENTER);
+      ellipseMode(RADIUS);
+      image(this.image,0,0,this.radius*2,this.radius*2);
+      pop();
+    }
+  }
